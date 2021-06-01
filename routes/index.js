5ebbed77ed4ee1a64fca3ffdb4f5d3891a05e9a8
Evgeniy Tarasov <tarasovem@gmail.com>
2021-06-01 23:20:44 +0300

    Исправляет путь к хранилищу выгруженных файлов

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrlFiles = require('../controllers/files');
 const multer = require('multer');
-const upload = multer({dest: '../uploads'});
+const upload = multer({dest: 'uploads'});
 
 router.post('/file', upload.single('foo'), ctrlFiles.fileCreate);
 
